4f806b867a86a1ffed5d59175b1e3e48f690aa9e
Karlaleticiaavila <Kbliaass@gmail.com>
2025-11-21 20:15:21 -0800

    exercise catch and async

--- file_reader.js
+++ file_reader.js
@@ -1,10 +1,39 @@
 const fs = require("fs").promises;
 
-// THEN-CATCH SOLUTION BELOW THIS LINE
+// THEN-CATCH
+fs.readFile("./firstname.txt", "utf-8") 
+.then(firstname => {
+    return fs.readFile("./lastname.txt", "utf-8")
+    .then(lastname => {
+        return fs.readFile("./age.txt", "utf-8")
+        .then(age => {
+            return fs.readFile("./hobbies.txt", "utf-8")
+            .then(hobbies => {
+                const hobbiesArr = hobbies.split(","); 
+                console.log(`Then-Catch: ${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbiesArr[0]} and ${hobbiesArr[1]}`);
+            });
+        });
+    });
+})
+.catch(err => {
+    console.error("Error:", err);
+});
 
 
+// ASYNC/AWAIT
+async function readFiles() {
+    try {
+        const firstname = await fs.readFile("./firstname.txt", "utf-8");
+        const lastname = await fs.readFile("./lastname.txt", "utf-8");
+        const age = await fs.readFile("./age.txt", "utf-8");
+        const hobbies = await fs.readFile("./hobbies.txt", "utf-8");
+        const hobbiesArr = hobbies.split(",");
 
+        console.log(`Async/Await: ${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbiesArr[0]} and ${hobbiesArr[1]}`);
+    } catch (err) {
+        console.error("Error:", err);
+    }
+}
 
+readFiles();
 
-
-// ASYNC/AWAIT SOLUTION BELOW THIS LINE
